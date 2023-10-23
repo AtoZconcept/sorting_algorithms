757dd72ec0f2c3718d579b7a3feaeafd454627e7
Mainagithubrit <njorogefrancismaina@gmail.com>
2023-10-23 15:56:31 +0000

    2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,53 @@
+#include "sort.h"
+
+/**
+ * swap - A function that swaps two intergers in an array
+ * @array: The array to swap from
+ * @min: The minimun integer in our array
+ * @index: The index of the array
+ */
+
+void swap(int *array, unsigned int index, unsigned int min)
+{
+	int tmp;
+	
+	tmp = array[index];
+	array[index] = array[min];
+	array[min] = tmp;
+}
+
+/**
+ * selection_sort - a function that sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ * @array: The array to sort
+ * @size: The size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j, min;
+	/**int flag;**/
+
+	if (array == NULL)
+		return;
+
+	for(i = 0; i < size; i++)
+	{
+		min = i;
+		/**flag = 0;**/
+		for(j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+				/**flag += 1;**/
+			}
+		}
+	if(min != i)
+	{
+		swap(array, i, min);
+		/**if(flag != 0)**/
+		print_array(array, size);
+	}
+}
+}
